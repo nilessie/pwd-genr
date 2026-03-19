48e8512569529f6dd98e3b543a132110a5d86f99
nilessie <257730676+nilessie@users.noreply.github.com>
2026-03-19 10:25:47 +0100

    Added constants for password constraints

---
+++ constants.py
@@ -0,0 +1,12 @@
+import string
+
+# Allowed characters
+SPECIAL_CHARS = string.punctuation
+NUMERIC_CHARS = string.digits
+LOWERCASE_LETTER_CHARS = string.ascii_uppercase
+UPPERCASE_LETTER_CHARS = string.ascii_uppercase
+
+# Password length limits
+MIN_LENGTH = 8
+MAX_LENGTH = 128
+
