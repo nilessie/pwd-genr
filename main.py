93388ff61220fa04b6562775b33079b3139753bb
nilessie <257730676+nilessie@users.noreply.github.com>
2026-03-19 14:27:01 +0100

    Added some colouring and made minor tweaks.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from constants import SPECIAL_CHARS, NUMERIC_CHARS
 from constants import LOWERCASE_LETTER_CHARS, UPPERCASE_LETTER_CHARS
 from constants import MIN_LENGTH, MAX_LENGTH
+from ascii_art import ascii_art
+from rich.console import Console
 import secrets
+
+console = Console()
 
 
 def main():
+    for line in ascii_art:
+        console.print(line, style="purple")
+    
     pwd_length = get_pwd_length()
     pwd_type = get_pwd_type()    
     
@@ -13,7 +20,7 @@ def main():
     if pwd_type == "r":
         gened_pwd = generate_rand_pwd(pwd_length)
 
-    print(f"your generated password is: {gened_pwd}")
+    console.print(f"\n[yellow]your generated password is:[/yellow] [blue]{gened_pwd}")
 
 
 
@@ -21,22 +28,22 @@ def main():
 def get_pwd_length():
     while True:
         try:
-            pwd_length = int(input(f"Choose a password length between {MIN_LENGTH} and {MAX_LENGTH}: "))
+            pwd_length = int(console.input(f"[yellow]Choose a password length between {MIN_LENGTH} and {MAX_LENGTH}: "))
             if pwd_length >= MIN_LENGTH and pwd_length <= MAX_LENGTH:
                 return pwd_length
             else:
-                print(f"Error: Input must be a number between {MIN_LENGTH} and {MAX_LENGTH}.")
+                console.print(f"[red]Error: Input must be a number between {MIN_LENGTH} and {MAX_LENGTH}.")
         except ValueError:
-            print(f"Error: Input must be a number.")
+            console.print(f"Error: Input must be a number.", style="red")
         
 # Prompt two option: pure random or custom
 def get_pwd_type():
     while True:
-        pwd_type = input("Random [r] or Custom [c]? ").lower()
+        pwd_type = console.input("\n[yellow]Random (r) or Custom (c)? ").lower()
         if pwd_type == "r" or pwd_type == "c":
             return pwd_type        
         else: 
-            print("Error: Invalid input. Input must be either r or c.")
+            console.print("Error: Invalid input. Input must be either r or c.", style="red")
         
 # Generate password based on the pwd_type (custom or random)
 def generate_custom_pwd(pwd_length):
@@ -50,12 +57,12 @@ def generate_custom_pwd(pwd_length):
 
     # Prompt user for the chars they want
     while True:
-        usr_conf_options = input(
-            "Special characters [1]\n"
-            "Numeric characters [2]\n"
-            "UPPER CASE CHARACTERS [3]\n"
-            "lower case characters [4]\n"
-            "What characters do you wish to include: "
+        usr_conf_options = console.input(
+            "\n[orange1]Special characters (1)\n"
+            "Numeric characters (2)\n"
+            "UPPER CASE CHARACTERS (3)\n"
+            "lower case characters (4)\n"
+            "[yellow]What characters do you wish to include: "
             )     
         
         # Create custom string to generate a password
